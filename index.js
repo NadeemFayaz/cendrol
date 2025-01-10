a92f8d2c4f43a0cf16dda1465613f9ace76c3795
Nadeem Fayaz <iamabnadeem99@gmail.com>
2025-01-10 22:40:49 +0530

    Added path

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const userRoutes = require('./routes/userRoutes.js');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+    res.send("Welcome to the Node.js Application!");
+});
+
+
 // Connect to database
 connectDB();
 
